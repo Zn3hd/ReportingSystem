f421bec18d7e3b8b26eb4bdaa40a500fecc5a214
zn3hd <xoxo9369@gmail.com>
2023-10-20 02:19:36 +0800

    first commit

---
+++ bullyingAndHarassmentReportingSystem/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\ReportController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/incidentReportForm', function () {
+    return view('incidentReportForm');
+});
+
+Route::get('/Reports', [ ReportController::class, 'index'])->name('report.index');
+Route::get('/Reports/incidentReportForm', [ ReportController::class, 'incidentReportForm'])->name('report.incidentReportForm');
+Route::post('/Reports', [ ReportController::class, 'store'])->name('report.store');
